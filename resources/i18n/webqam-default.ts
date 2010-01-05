b1ec0fb3f51a86ba61cee868d43caa5dbd218e19
slux83 <slux83@08b609cc-8b46-11de-b79f-03808d50d0c5>
2010-01-05 19:30:49 +0000

    Init i18n support

---
+++ resources/i18n/webqam-default.ts
@@ -0,0 +1,367 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="ui/aboutdialog.ui" line="14"/>
+        <source>About webQam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/aboutdialog.ui" line="24"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:8pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:28pt; font-weight:600; font-style:italic; color:#932323;&quot;&gt;web&lt;/span&gt;&lt;img src=&quot;:/icons/logoQ&quot; /&gt;&lt;span style=&quot; font-size:28pt; font-weight:600; font-style:italic; color:#932323;&quot;&gt;am&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Version:&lt;/span&gt; %version%&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Author:&lt;/span&gt; Alessio &apos;Slux&apos; Di Fazio &amp;lt;&lt;a href=&quot;mailto:slux83@gmail.com&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;slux83@gmail.com&lt;/span&gt;&lt;/a&gt;&amp;gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;License:&lt;/span&gt; GPLv3&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Icons:&lt;/span&gt; &lt;a href=&quot;http://www.oxygen-icons.org/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Oxygen&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Website:&lt;/span&gt; &lt;a href=&quot;http://code.google.com/p/webqam/&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://code.google.com/p/webqam/&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AddFolderDialog</name>
+    <message>
+        <location filename="src/gui/addfolderdialog.cpp" line="80"/>
+        <source>webQam - Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addfolderdialog.ui" line="17"/>
+        <source>webQam - Add Folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addfolderdialog.ui" line="27"/>
+        <source>Folder name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AddWebcamDialog</name>
+    <message>
+        <location filename="src/gui/addwebcamdialog.cpp" line="104"/>
+        <source>webQam - Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/addwebcamdialog.cpp" line="123"/>
+        <source>Image not valid or not loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addwebcamdialog.ui" line="14"/>
+        <source>webQam - Add webcam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addwebcamdialog.ui" line="24"/>
+        <source>Webcam URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addwebcamdialog.ui" line="47"/>
+        <source>Webcam name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addwebcamdialog.ui" line="54"/>
+        <source>Webcam preview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addwebcamdialog.ui" line="113"/>
+        <source>Network request config</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/addwebcamdialog.ui" line="122"/>
+        <source>Refer URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CamsController</name>
+    <message>
+        <location filename="src/global/camscontroller.cpp" line="159"/>
+        <source>Folder name allready exists! Please choose another one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/global/camscontroller.cpp" line="162"/>
+        <location filename="src/global/camscontroller.cpp" line="189"/>
+        <source>Unknown error: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/global/camscontroller.cpp" line="186"/>
+        <source>Url allready exists! Please choose another one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ImageMenu</name>
+    <message>
+        <location filename="src/gui/imagemenu.cpp" line="29"/>
+        <source>Click to enlarge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/imagemenu.cpp" line="66"/>
+        <source>Last update: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/imagemenu.cpp" line="70"/>
+        <source>Image not valid or not loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="src/main.cpp" line="32"/>
+        <location filename="src/main.cpp" line="41"/>
+        <source>webQam - critical</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="33"/>
+        <source>Your system doesn&apos;t supports system tray. Aborting...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="42"/>
+        <source>No database drivers found! This application requires SQLITE Qt database driver. Please check the documentation how to build the QSQLITE plugin.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="57"/>
+        <source>webQam - Cannot execute query</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/main.cpp" line="58"/>
+        <source>Unable to create database main structure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/model/tree/treemodelfactory.cpp" line="32"/>
+        <source>Webcam name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/model/tree/treemodelfactory.cpp" line="32"/>
+        <source>Webcam source</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsGui</name>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="44"/>
+        <source>Add folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="45"/>
+        <source>Add webcam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="83"/>
+        <location filename="src/gui/settingsgui.cpp" line="171"/>
+        <source>webQam - question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="83"/>
+        <source>Do you want to save changed options?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="125"/>
+        <source>Property saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="146"/>
+        <source>Options saved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="166"/>
+        <source>webQam - Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="166"/>
+        <source>Please select webcams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/settingsgui.cpp" line="171"/>
+        <source>Delete selected items?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="14"/>
+        <source>webQam options...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="29"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="38"/>
+        <source>Refresh timeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="51"/>
+        <source> minute(s)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="70"/>
+        <source>Thumbnail width size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="83"/>
+        <source>px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="103"/>
+        <source>Webcams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="111"/>
+        <location filename="ui/settingsgui.ui" line="124"/>
+        <source>Filter webcams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="131"/>
+        <source>Clear filter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="152"/>
+        <source>Remove selected webcams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="166"/>
+        <source>Add new webcam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/settingsgui.ui" line="229"/>
+        <source>Save changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemTray</name>
+    <message>
+        <location filename="src/gui/systemtray.cpp" line="67"/>
+        <source>Webcams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/systemtray.cpp" line="91"/>
+        <source>Options...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/systemtray.cpp" line="92"/>
+        <source>About webQam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/systemtray.cpp" line="95"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WebcamViewer</name>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="87"/>
+        <source>Image size: %1x%2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="96"/>
+        <source>Image not valid or not loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="115"/>
+        <location filename="ui/webcamviewer.ui" line="127"/>
+        <source>Save Image As...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="117"/>
+        <source>Images (*.png)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="123"/>
+        <source>ERROR: Cannot save image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="125"/>
+        <source>Image saved.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/gui/webcamviewer.cpp" line="134"/>
+        <source>Image size: %1x%2 (%3%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/webcamviewer.ui" line="14"/>
+        <source>webQam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/webcamviewer.ui" line="65"/>
+        <source>Zoom 100%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/webcamviewer.ui" line="82"/>
+        <source>px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/webcamviewer.ui" line="85"/>
+        <source>Image width: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/webcamviewer.ui" line="107"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/webcamviewer.ui" line="118"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
